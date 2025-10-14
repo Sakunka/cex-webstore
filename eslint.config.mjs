fee9a2c9f6befc80890d56baa47b2af78e99e368
Aleksandar Radosevic <radosevic145@gmail.com>
2025-10-14 18:19:07 +0200

    Promijeni eslint pravila

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,7 +21,6 @@ const eslintConfig = [
     ],
   },
   {
-    extends: ["next/core-web-vitals"],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
